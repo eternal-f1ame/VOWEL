f1db90ec53f1014f501b0f2a5999a5599a74a436
eternal-f1ame <thunder.tanay.1907@gmail.com>
2022-07-07 15:08:14 +0530

    Create hands.py

---
+++ my_robot/python_scripts/hands.py
@@ -0,0 +1,106 @@
+from operator import mod
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# For static images:
+IMAGE_FILES = []
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+  for idx, file in enumerate(IMAGE_FILES):
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv2.flip(cv2.imread(file), 1)
+    # Convert the BGR image to RGB before processing.
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # Print handedness and draw hand landmarks on the image.
+    print('Handedness:', results.multi_handedness)
+    if not results.multi_hand_landmarks:
+      continue
+    image_height, image_width, _ = image.shape
+    annotated_image = image.copy()
+    for hand_landmarks in results.multi_hand_landmarks:
+      print('hand_landmarks:', hand_landmarks)
+      print(
+          f'Index finger tip coordinates: (',
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+      )
+      mp_drawing.draw_landmarks(
+          annotated_image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+    cv2.imwrite(
+        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    if not results.multi_hand_world_landmarks:
+      continue
+    for hand_world_landmarks in results.multi_hand_world_landmarks:
+      mp_drawing.plot_landmarks(
+        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+
+# For webcam input:
+cap = cv2.VideoCapture(-1)
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    h, w, c = image.shape
+    x_max = 0
+    y_max = 0
+    x_min = w
+    y_min = h
+
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        for landmark in hand_landmarks.landmark:
+          x = int(landmark.x * w)
+          y = int(landmark.y * h)
+          if x > x_max:
+            x_max = x
+          if y > y_max:
+            y_max = y
+          if x < x_min:
+            x_min = x
+          if y < y_min:
+            y_min = y
+  
+        x_l =(x_min+x_max)//2
+        y_l =(y_min+y_max)//2
+        cv2.rectangle(
+          image,
+          (x_min-int(abs(0.2*x_l)),
+          y_min-int(abs(0.2*y_l))),
+          (x_max+int(abs(0.2*x_l)),
+          y_max+int(abs(0.2*y_l))),
+          (0, 255, 0), 2)
+
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+
+cap.release()
